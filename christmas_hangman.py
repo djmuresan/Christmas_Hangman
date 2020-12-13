c2431bd5e0cf627b890e7258093efea6c787a0ed
djmuresan <65506631+djmuresan@users.noreply.github.com>
2020-12-12 22:58:26 -0800

    Update christmas_hangman.py

--- christmas_hangman.py
+++ christmas_hangman.py
@@ -1,6 +1,6 @@
 import random
 
-print("This is a holiday themed hangman. Try to find the word based of the dificulty you choose")
+print("This is a holiday-themed hangman. Try to find the word based of the difficulty you choose")
 
 words = ['bells', 'cards', 'hymns', 'elves', 'grace', 'merry', 'feast', 'aroma', 'green', 'jesus', 'faith']
 easy_words = ['elf', 'cap', 'eat', 'ask', 'eve', 'pew', 'bow', 'ate', 'box', 'awe', 'fig', 'nap', 'lap', 'fly', 'tag', 'fir', 'ice']
@@ -10,14 +10,14 @@ clue = list('?????')
 clue_easy = list('???')
 clue_hard = list('???????')
 
-dffclty = input('Choose difficulty type (type 1, 2, or 3)\n 1 Easy\n 2 Normal\n 3 Hard\n : ')
-dffclty = int(dffclty)
+diffculty = input('Choose difficulty type (type 1, 2, or 3)\n 1 Easy\n 2 Normal\n 3 Hard\n : ')
+diffculty = int(diffculty)
 
-if dffclty == 1:
+if diffculty == 1:
     lives = 7
     clue = clue_easy
     words = easy_words
-elif dffclty == 2:
+elif diffculty == 2:
     lives = 6
 else:
     lives = 5
@@ -27,12 +27,15 @@ else:
 scrt_wrd = random.choice(words)
 hrt = u'\u2764'
 gssd_wrd_crrctly = False
+count = 0
 
 def update_clue(gssd_lttr, scrt_wrd, clue):
+    global count
     index = 0
     while index < len(scrt_wrd):
         if gssd_lttr == scrt_wrd[index]:
             clue[index] = gssd_lttr
+            count += 1
         index += 1
 
 while lives > 0:
@@ -47,11 +50,14 @@ while lives > 0:
     if guess in scrt_wrd:
         update_clue(guess, scrt_wrd, clue)
         print('That is a letter here, good job.')
+        if count == len(scrt_wrd):
+            gssd_wrd_crrctly = True
+            break
     else:
         print('Incorrect, You lose a life. Try Again!  ')
         lives -= 1
 
 if gssd_wrd_crrctly is True:
-    print('Merry Christmas, You Won! the word was ' + scrt_wrd)
+    print(f"Merry Christmas, You Won! the word was '{scrt_wrd}'.")
 else:
     print('Bummer, You Lost! the word was ' + scrt_wrd)
